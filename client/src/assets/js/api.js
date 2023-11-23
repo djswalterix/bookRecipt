bdf8d232f3037f0fa98ced5ac4d31b271a41b3c5
djswalterix <walter45hoxha@gmail.com>
2023-11-23 16:13:41 +0100

    dotenv

--- client/src/assets/js/api.js
+++ client/src/assets/js/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const dotenv = require("dotenv");
+import dotenv from ("dotenv");
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000",
 });
