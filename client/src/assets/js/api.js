07b0545ab2e5f2ed7d5d4af0901f53ffd31017a9
djswalterix <walter45hoxha@gmail.com>
2023-11-23 17:21:39 +0100

    for dev

--- client/src/assets/js/api.js
+++ client/src/assets/js/api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.REACT_APP_API_URL ||
-    "https://bookrecip-e5a5d3bc5103.herokuapp.com/",
+  baseURL: "https://bookrecip-e5a5d3bc5103.herokuapp.com/",
 });
-
 export default axiosInstance;
--- server/config/cors.js
+++ server/config/cors.js
@@ -1,6 +1,7 @@
 const cors = require("cors");
 const corsOptions = {
-  origin: "https://astounding-madeleine-5192af.netlify.app", // Sostituisci con il tuo dominio Netlify
+  //origin: "https://astounding-madeleine-5192af.netlify.app",
+  origin: "/", // Sostituisci con il tuo dominio Netlify
   optionsSuccessStatus: 200, // Per browser legacy che non supportano CORS
 };
 
