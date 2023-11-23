43ec583715fa9d3a1da13c175089b00f92771aeb
djswalterix <walter45hoxha@gmail.com>
2023-11-23 22:02:55 +0100

    hheroku

--- server/config/db.js
+++ server/config/db.js
@@ -4,6 +4,12 @@ const fs = require("fs"); // Require the fs module to read the SSL certificate f
 
 dotenv.config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_NAME:", process.env.NODE_ENV ? "test" : process.env.DB_NAME);
+
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: process.env.DB_HOST,
