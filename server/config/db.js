22be71e19499d997722f573844e8a1f189b7f63d
djswalterix <walter45hoxha@gmail.com>
2023-11-27 21:53:04 +0100

    for deploy

--- server/config/db.js
+++ server/config/db.js
@@ -5,12 +5,6 @@ const fs = require("fs"); // Require the fs module to read the SSL certificate f
 dotenv.config();
 const dbName = process.env.NODE_ENV === "test" ? "test" : "defaultdb";
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_PORT:", process.env.DB_PORT);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_NAME:", dbName);
-
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: process.env.DB_HOST,
