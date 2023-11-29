8f381830a2720cbff98b47f38a3e8429eff0b5d2
djswalterix <walter45hoxha@gmail.com>
2023-11-29 01:15:27 +0100

    deploy

--- client/src/assets/js/api.js
+++ client/src/assets/js/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-const dotenv = require("dotenv");
-dotenv.config();
+
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://book-receipt-1.lm.r.appspot.com/",
   //baseURL: "http://localhost:3000",
 });
 /*
