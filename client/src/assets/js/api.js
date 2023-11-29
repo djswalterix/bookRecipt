cf16d09cae922025db9c51fed1255ee4229d07bd
djswalterix <walter45hoxha@gmail.com>
2023-11-29 01:02:52 +0100

    env

--- client/src/assets/js/api.js
+++ client/src/assets/js/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://bookrecip-e5a5d3bc5103.herokuapp.com",
+  baseURL: process.env.REACT_APP_API_URL,
   //baseURL: "http://localhost:3000",
 });
 /*
